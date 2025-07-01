037f1e8a7b005b409fc0977c116948f88a60e5f7
D-Ohmzz <douglasomega13@gmail.com>
2025-07-01 10:21:58 +0300

    Category model class

--- ecomm/src/main/java/com/ecomm/ecomm/model/CategoryModel.java
+++ ecomm/src/main/java/com/ecomm/ecomm/model/CategoryModel.java
@@ -1,4 +1,27 @@
 package com.ecomm.ecomm.model;
 
 public class CategoryModel {
+    private Long id;
+    private String categoryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public CategoryModel(Long id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
