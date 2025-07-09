89edeb111588dbbebeecb3aaa95ceea3c17a05c4
D-Ohmzz <douglasomega13@gmail.com>
2025-07-09 14:29:35 +0300

    Used lombok annotations

--- ecomm/src/main/java/com/ecomm/ecomm/model/Category.java
+++ ecomm/src/main/java/com/ecomm/ecomm/model/Category.java
@@ -4,38 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.annotation.processing.Generated;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String categoryName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public Category(Long id, String categoryName) {
-        this.id = id;
-        this.categoryName = categoryName;
-    }
-
-    public Category(){
-
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
