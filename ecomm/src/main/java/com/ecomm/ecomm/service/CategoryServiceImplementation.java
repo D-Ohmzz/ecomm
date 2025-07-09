5501d4c1e3585bf949e3bd9f6ce34e9dc9785de1
D-Ohmzz <douglasomega13@gmail.com>
2025-07-09 10:01:03 +0300

    Switching to h2db from Array  List

--- ecomm/src/main/java/com/ecomm/ecomm/service/CategoryServiceImplementation.java
+++ ecomm/src/main/java/com/ecomm/ecomm/service/CategoryServiceImplementation.java
@@ -1,60 +1,73 @@
 package com.ecomm.ecomm.service;
 
 import com.ecomm.ecomm.model.Category;
+import com.ecomm.ecomm.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImplementation implements CategoryService {
 
-    private List<Category> categories = new ArrayList<>();
-    int intid;
-    long id;
+    //private List<Category> categories = new ArrayList<>();
+    //int intid;
+    //long id;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        //Implementing id auto generation
-        if (categories.isEmpty()) {
-            intid = 1;
-        }else {
-            intid++;
-        }
-        id = intid;
-        category.setId(id);
-        categories.add(category);
-
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long id) {
-        Category category = categories.stream()
-                .filter(c->c.getId().equals(id))
-                .findFirst()
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
+        /*List<Category> categories = categoryRepository.findAll();
 
-        categories.remove(category);
-        return " Category with id: "+id+" has been deleted successfully!";
+        Category category = categories.stream()
+        .filter(c->c.getId().equals(id))
+        .findFirst()
+        orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"))*/
+        ;
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isPresent()) {
+            categoryRepository.deleteById(id);
+            return " Category with id: " + id + " has been deleted successfully!";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found");
         }
+    }
 
     @Override
     public void updateCategory(Category category, Long id) {
-        Category categoryFromArrayList = categories.stream()
+        Optional<Category>optionalCategory= categoryRepository.findById(id);
+        if(optionalCategory.isPresent()){
+            Category categoryFromDb = optionalCategory.get();
+            categoryFromDb.setCategoryName(category.getCategoryName());
+            categoryRepository.save(categoryFromDb);
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found");
+        }
+
+        /*List<Category> categories = categoryRepository.findAll();
+          Category categoryFromDb = categories.stream()
                 .filter(c->c.getId().equals(id))
                 .findFirst()
                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
 
-        int index = categories.indexOf(categoryFromArrayList);
-        category.setId(id);
-        categories.set(index, category);
+               categoryRepository.save(category);*/
     }
 
 }
