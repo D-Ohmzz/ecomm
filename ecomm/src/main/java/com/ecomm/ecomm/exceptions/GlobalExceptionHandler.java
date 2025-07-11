3141d4a638a7b02e1938a853f56fd6fec1baf0fc
D-Ohmzz <douglasomega13@gmail.com>
2025-07-11 22:31:26 +0300

    Custom excpetion-Prevent duplicate category name

--- ecomm/src/main/java/com/ecomm/ecomm/exceptions/GlobalExceptionHandler.java
+++ ecomm/src/main/java/com/ecomm/ecomm/exceptions/GlobalExceptionHandler.java
@@ -28,4 +28,10 @@ public class GlobalExceptionHandler {
         String message = e.getMessage();
         return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String>apiException(APIException e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
 }
