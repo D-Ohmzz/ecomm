111e5d386aba57bb1da28823b7492e2a884411ef
D-Ohmzz <douglasomega13@gmail.com>
2025-07-14 16:49:40 +0300

    Returning Category Response DTO

--- ecomm/src/main/java/com/ecomm/ecomm/service/CategoryService.java
+++ ecomm/src/main/java/com/ecomm/ecomm/service/CategoryService.java
@@ -1,11 +1,12 @@
 package com.ecomm.ecomm.service;
 
 import com.ecomm.ecomm.model.Category;
+import com.ecomm.ecomm.payload.CategoryResponse;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
+    CategoryResponse getAllCategories();
     void createCategory(Category category);
     String deleteCategory(Long id);
     void updateCategory(Category category, Long id);
