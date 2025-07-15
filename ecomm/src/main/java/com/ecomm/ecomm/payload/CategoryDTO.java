525d3c91c5eec8b723d99b857e918d020c16a565
D-Ohmzz <douglasomega13@gmail.com>
2025-07-15 08:40:51 +0300

    Corrected long to Long

--- ecomm/src/main/java/com/ecomm/ecomm/payload/CategoryDTO.java
+++ ecomm/src/main/java/com/ecomm/ecomm/payload/CategoryDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
-    private long id;
+    private Long id;
     private String categoryName;
 
 }
