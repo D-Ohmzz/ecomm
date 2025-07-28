079a57101389f9c5c0f3c5f36409128d9d5e9026
D-Ohmzz <douglasomega13@gmail.com>
2025-07-28 15:55:27 +0300

    add product repository

--- ecomm/src/main/java/com/ecomm/ecomm/repository/ProductRepository.java
+++ ecomm/src/main/java/com/ecomm/ecomm/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ecomm.ecomm.repository;
 
 import com.ecomm.ecomm.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
