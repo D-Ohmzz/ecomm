920f437de45f9d72e89f6a4d4c5ebec5f54b28ce
D-Ohmzz <douglasomega13@gmail.com>
2025-07-29 15:10:36 +0300

    add find product by category

--- ecomm/src/main/java/com/ecomm/ecomm/repository/ProductRepository.java
+++ ecomm/src/main/java/com/ecomm/ecomm/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.ecomm.ecomm.repository;
 
+import com.ecomm.ecomm.model.Category;
 import com.ecomm.ecomm.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategory(Category category, Pageable pageDetails);
 }
