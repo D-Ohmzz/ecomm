f7e1dd6173f56eec84de615bebba221699e2b869
D-Ohmzz <douglasomega13@gmail.com>
2025-07-29 15:11:30 +0300

    refactor

--- ecomm/src/main/java/com/ecomm/ecomm/payload/ProductDTO.java
+++ ecomm/src/main/java/com/ecomm/ecomm/payload/ProductDTO.java
@@ -1,4 +1,19 @@
 package com.ecomm.ecomm.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
+    private Long id;
+    private String productName;
+    private String image;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
 }
