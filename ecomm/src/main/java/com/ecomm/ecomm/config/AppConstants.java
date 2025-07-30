3682e5e974956b9bd80cd37e0a1cc91a49c808fa
D-Ohmzz <douglasomega13@gmail.com>
2025-07-30 09:57:23 +0300

    add products pagination constants

--- ecomm/src/main/java/com/ecomm/ecomm/config/AppConstants.java
+++ ecomm/src/main/java/com/ecomm/ecomm/config/AppConstants.java
@@ -5,4 +5,6 @@ public class AppConstants {
     public static final String PAGE_SIZE = "50";
     public static final String CATEGORIES_SORT_BY = "id";
     public static final String CATEGORIES_SORT_ORDER = "asc";
+    public static final String PRODUCTS_SORT_BY = "id";
+    public static final String PRODUCTS_SORT_ORDER = "asc";
 }
