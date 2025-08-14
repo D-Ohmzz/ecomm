4d53f2687eef1e15e17af630203030f16928ad76
D-Ohmzz <douglasomega13@gmail.com>
2025-08-14 15:58:17 +0300

    added role model class

--- ecomm/src/main/java/com/ecomm/ecomm/model/Role.java
+++ ecomm/src/main/java/com/ecomm/ecomm/model/Role.java
@@ -1,4 +1,22 @@
 package com.ecomm.ecomm.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private AppRole roleName;
+
+    public Role(AppRole roleName){
+        this.roleName=roleName;
+    }
 }
