eaa94bd846b90eea0666b498712960322b66e161
D-Ohmzz <douglasomega13@gmail.com>
2025-08-15 12:47:01 +0300

    implemented users, addresses and user_address join table

--- ecomm/src/main/java/com/ecomm/ecomm/model/Address.java
+++ ecomm/src/main/java/com/ecomm/ecomm/model/Address.java
@@ -1,13 +1,49 @@
 package com.ecomm.ecomm.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "addresses")
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(min = 10, max = 30)
+    private String address;
+
+    @NotBlank
+    @Size(min = 5, max = 30)
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 5, max = 30)
+    private String city;
+
+    @NotBlank
+    @Size(max = 30)
+    private String country;
+
+    public Address(String address, String buildingName, String city, String country) {
+        this.address = address;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.country = country;
+    }
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
 }
