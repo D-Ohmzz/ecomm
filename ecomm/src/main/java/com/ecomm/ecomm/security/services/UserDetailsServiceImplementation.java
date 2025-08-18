fe6b27deea826cd0e63bc4e479a3d90d8937fc38
D-Ohmzz <douglasomega13@gmail.com>
2025-08-18 16:27:45 +0300

    added custom user details service implementation

---
+++ ecomm/src/main/java/com/ecomm/ecomm/security/services/UserDetailsServiceImplementation.java
@@ -0,0 +1,93 @@
+package com.ecomm.ecomm.security.services;
+
+import com.ecomm.ecomm.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@Data
+public class UserDetailsServiceImplementation implements UserDetails {
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    public UserDetailsServiceImplementation(long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsServiceImplementation build(User user){
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                .collect(Collectors.toList());
+        return new UserDetailsServiceImplementation(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+    private Collection<? extends GrantedAuthority> authorities;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public boolean equals (Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()){
+            return false;
+        }
+        UserDetailsServiceImplementation user = (UserDetailsServiceImplementation) o;
+        return Objects.equals(id, user.id);
+    }
+}
