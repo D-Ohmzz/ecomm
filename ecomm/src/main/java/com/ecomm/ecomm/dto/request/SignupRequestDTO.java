b506168fbc1fcd0202119a995c1753a67ae2d248
D-Ohmzz <douglasomega13@gmail.com>
2025-08-21 15:06:28 +0300

    added signup request dto

--- ecomm/src/main/java/com/ecomm/ecomm/dto/request/SignupRequestDTO.java
+++ ecomm/src/main/java/com/ecomm/ecomm/dto/request/SignupRequestDTO.java
@@ -1,4 +1,22 @@
 package com.ecomm.ecomm.dto.request;
 
-public class SignupRequestDTO {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+public record SignupRequestDTO(
+        @NotBlank(message="Username field cannot be blank !!!")
+        @Size(min = 3, max = 15)
+        String username,
+        @NotBlank(message="Email field cannot be blank !!!")
+        @Size(max = 50)
+        @Email
+        String email,
+        @NotBlank(message="Password field cannot be blank !!!")
+        @Size(min = 5, max = 25)
+        String password,
+        Set<String> roles
+
+){}
