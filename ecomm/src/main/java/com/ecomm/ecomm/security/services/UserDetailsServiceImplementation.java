58a08905222a9b2d03aa6ad79646b468a4691a33
D-Ohmzz <douglasomega13@gmail.com>
2025-08-25 11:22:01 +0300

    refactored the loadUserByUsername method

--- ecomm/src/main/java/com/ecomm/ecomm/security/services/UserDetailsServiceImplementation.java
+++ ecomm/src/main/java/com/ecomm/ecomm/security/services/UserDetailsServiceImplementation.java
@@ -2,11 +2,12 @@ package com.ecomm.ecomm.security.services;
 
 import com.ecomm.ecomm.model.User;
 import com.ecomm.ecomm.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserDetailsServiceImplementation implements UserDetailsService {
@@ -14,14 +15,14 @@ public class UserDetailsServiceImplementation implements UserDetailsService {
     public UserDetailsServiceImplementation(UserRepository userRepository){
         this.userRepository=userRepository;
     }
-
-
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(()->
-                        new UsernameNotFoundException("User Not Found with username: "+username));
-        return UserDetailsImplementation.build(user);
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if(optionalUser.isPresent()){
+            User user = optionalUser.get();
+            return UserDetailsImplementation.build(user);
+        }else{
+            throw new UsernameNotFoundException("User Not Found with username: "+username);
+        }
     }
 }
