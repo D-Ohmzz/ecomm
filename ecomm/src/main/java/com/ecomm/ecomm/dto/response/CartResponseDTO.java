484e28f8616d84a6c72614db7fdadbd841acaa09
D-Ohmzz <douglasomega13@gmail.com>
2025-09-17 16:46:45 +0300

    added cart response DTO class

--- ecomm/src/main/java/com/ecomm/ecomm/dto/response/CartResponseDTO.java
+++ ecomm/src/main/java/com/ecomm/ecomm/dto/response/CartResponseDTO.java
@@ -1,4 +1,10 @@
 package com.ecomm.ecomm.dto.response;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartResponseDTO {
+    private Long id;
+    private Double totalPrice = 0.0;
+    private List<ProductResponseDTO> products = new ArrayList<>();
 }
