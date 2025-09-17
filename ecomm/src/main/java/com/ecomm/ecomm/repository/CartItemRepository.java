e7c5d1105be4759eafb4fe5a11b1898b9a88a1cb
D-Ohmzz <douglasomega13@gmail.com>
2025-09-17 16:44:56 +0300

    added Cart Item repository class

---
+++ ecomm/src/main/java/com/ecomm/ecomm/repository/CartItemRepository.java
@@ -0,0 +1,7 @@
+package com.ecomm.ecomm.repository;
+
+import com.ecomm.ecomm.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
