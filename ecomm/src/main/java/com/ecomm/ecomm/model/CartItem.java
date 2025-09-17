6bd5d1f15432306076f1656a0e33381f3ad7b3a6
D-Ohmzz <douglasomega13@gmail.com>
2025-09-17 12:24:59 +0300

    added cart item model

--- ecomm/src/main/java/com/ecomm/ecomm/model/CartItem.java
+++ ecomm/src/main/java/com/ecomm/ecomm/model/CartItem.java
@@ -1,4 +1,30 @@
 package com.ecomm.ecomm.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "cart_items")
+@Table(name = "cart_items")
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer Quantity;
+
+    private Double productPrice;
+
 }
