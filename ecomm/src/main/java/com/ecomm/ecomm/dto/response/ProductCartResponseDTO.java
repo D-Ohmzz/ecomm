dff16182820c8456434bacf91313fcda69e430c9
D-Ohmzz <douglasomega13@gmail.com>
2025-09-22 11:48:57 +0300

    changed from class to record

--- ecomm/src/main/java/com/ecomm/ecomm/dto/response/ProductCartResponseDTO.java
+++ ecomm/src/main/java/com/ecomm/ecomm/dto/response/ProductCartResponseDTO.java
@@ -1,4 +1,12 @@
 package com.ecomm.ecomm.dto.response;
 
-public class ProductCartResponseDTO {
-}
+public record ProductCartResponseDTO(
+        Long id,
+        String productName,
+        String image,
+        String description,
+        Integer quantity,
+        Double price,
+        Double discount,
+        Double specialPrice
+){}
