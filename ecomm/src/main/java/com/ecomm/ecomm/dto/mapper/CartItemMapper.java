5376846eb93a58439da015653a6994dd803c58ba
D-Ohmzz <douglasomega13@gmail.com>
2025-09-22 11:54:58 +0300

    added cart item mapper

--- ecomm/src/main/java/com/ecomm/ecomm/dto/mapper/CartItemMapper.java
+++ ecomm/src/main/java/com/ecomm/ecomm/dto/mapper/CartItemMapper.java
@@ -1,4 +1,14 @@
 package com.ecomm.ecomm.dto.mapper;
 
+import com.ecomm.ecomm.dto.response.CartItemResponseDTO;
+import com.ecomm.ecomm.dto.response.CartResponseDTO;
+import com.ecomm.ecomm.dto.response.ProductCartResponseDTO;
+import com.ecomm.ecomm.model.CartItem;
+
+import java.util.List;
+
 public class CartItemMapper {
+    public CartItemResponseDTO convertToCartResponseDTO (CartItem cartItem, List<CartResponseDTO> cartResponseDTOS, List<ProductCartResponseDTO> productCartResponseDTOs){
+        return new CartItemResponseDTO(cartItem.getId(), cartResponseDTOS, productCartResponseDTOs,cartItem.getQuantity(), cartItem.getDiscount(), cartItem.getProductPrice());
+    }
 }
