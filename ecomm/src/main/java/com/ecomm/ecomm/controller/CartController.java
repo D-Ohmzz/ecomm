6593e7f91eea5106fa95ec6858269574303ebe19
D-Ohmzz <douglasomega13@gmail.com>
2025-09-22 11:56:21 +0300

    added cart controller class

--- ecomm/src/main/java/com/ecomm/ecomm/controller/CartController.java
+++ ecomm/src/main/java/com/ecomm/ecomm/controller/CartController.java
@@ -1,4 +1,31 @@
 package com.ecomm.ecomm.controller;
 
+import com.ecomm.ecomm.dto.response.CartResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class CartController {
+    private CartService cartService;
+    public CartController(CartService cartService){
+        this.cartService=cartService;
+    }
+    @PostMapping("/public/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartResponseDTO> addProductToCart(@PathVariable Long productId,
+                                                            @PathVariable Integer quantity){
+        CartResponseDTO cartResponseDTO = cartService.addProductToCart(productId, quantity);
+        //Map<String, String> map = new HashMap<>();
+        //map.put("message", "Product added to the cart successfully!!!");
+        //map.put("status", "true");
+        return new ResponseEntity<>(cartResponseDTO, HttpStatus.CREATED);
+    }
+
 }
