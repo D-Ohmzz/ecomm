09dbee2e73076754cdea9e8f3d5d9a78d0779a69
D-Ohmzz <douglasomega13@gmail.com>
2025-09-22 11:55:27 +0300

    added cart mapper

--- ecomm/src/main/java/com/ecomm/ecomm/dto/mapper/CartMapper.java
+++ ecomm/src/main/java/com/ecomm/ecomm/dto/mapper/CartMapper.java
@@ -1,4 +1,15 @@
 package com.ecomm.ecomm.dto.mapper;
 
+import com.ecomm.ecomm.dto.response.CartResponseDTO;
+import com.ecomm.ecomm.dto.response.ProductCartResponseDTO;
+import com.ecomm.ecomm.model.Cart;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class CartMapper {
+    public CartResponseDTO convertToCartResponseDTO(Cart cart, List<ProductCartResponseDTO> productCartResponseDTOs){
+        CartResponseDTO cartResponseDTO = new CartResponseDTO(cart.getId(), cart.getTotalPrice(), productCartResponseDTOs );
+    }
 }
